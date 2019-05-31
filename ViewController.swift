dd8c02c02e53b853a6be83d803223d4bf420fcd7
Artem <inste@icloud.com>
2019-05-31 21:47:03 +0300

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UICollectionViewWithAnimationRows
+//
+//  Created by Артём Кармазь on 5/31/19.
+//  Copyright © 2019 Artem Karmaz. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
